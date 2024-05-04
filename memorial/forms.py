76501994b94785ba320d54ac01934f78df200069
Sandra <99267898+Sandra-Silva-Santos@users.noreply.github.com>
2024-05-03 21:37:30 -0300

    Delete directory

--- memorial/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Memorial
-
-class MemorialForm(forms.ModelForm):
-    class Meta:
-        model = Memorial
-        fields = '__all__'
-
-               
